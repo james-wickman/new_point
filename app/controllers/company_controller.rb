13038f2fc2a40c8394f73e8812ef202697843e1d
nauticalnova <nauticalnova@hotmail.com>
2017-05-12 14:08:26 -0600

    basic sidebar on map

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -11,7 +11,9 @@ class CompanyController < ApplicationController
   	@user_location =  [0, 0] #[request.location.latitude, request.location.longitude] not working on some internets
 
   	#distributers list should be formated [distributer name, distributer address, distributer phone, url]
-  	@distributers_list = [['ADI', '1234 e 1st ave salt lake city utah 84108', '123-456-7890', 'https://www.adi.com'], ['ADI', '1234 w 1st ave sandy utah 84070', '123-456-7890', 'https://www.adi.com'], ['ADI', '100 n 300 e american fork, utah 84003', '123-456-7890', 'https://www.adi.com']]
+  	@distributers_list = [['ADI', '1234 e 1st ave salt lake city utah 84108', '123-456-7890', 'https://www.adi.com'], 
+                          ['ADI', '1234 w 1st ave sandy utah 84070', '123-456-7890', 'https://www.adi.com'], 
+                          ['ADI', '100 n 300 e american fork, utah 84003', '123-456-7890', 'https://www.adi.com']]
     
   	@distributers_locations = []
   	@distributers_list.each do |dist|
